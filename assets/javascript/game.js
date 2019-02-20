2a7b2b1edea8c17799fad5128f84f09abb30e4c3
unknown <exumcodes@gmail.com>
2019-02-19 20:27:26 -0700

    Adding complete game.js. Thanks Jeeeeeffff!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 // Creates an array that stores all the options (A - Z).
-var letters = ['a'];
-var userGuess = [];
-var compGuess = '';
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userGuess;
+// // Remember to "label" your console logs. 
+// console.log("userGuess is " + userGuess);
+var compGuess;
 // Create the variables that store the number of wins, losses, and guesses left. Wins & loses start at 0 and increase by 1. Guesses left start at 7 and decrease by 1.
 var wins = 0;
 var losses = 0;
@@ -12,32 +14,49 @@ var setComGuess = function() {
 compGuess = letters[Math.floor(Math.random()* letters.length)];
 console.log(compGuess);
 }
+// Declaring the resetGame function for later calls.
+function resetGame() {
+    setComGuess();
+    guessesLeft = 7;
+    document.querySelector('#guessesLeft').textContent = guessesLeft;
+    document.querySelector("#yourGuesses").textContent = "";
+}
+// Calling the setComGuess function () declared above. *SCOPE STUFF --> Runs before everything*
+setComGuess();
 
 // Function for whenever user presses a key.
 document.onkeyup = function(event) {
     // console.log(event);
-    userGuess.push(event.key);
+    // Limiting the userGuess to the letters array using the "includes" array prototype method...what is a prototype IDK, just do it.
+    // IF-ELSE SHORTCUT (ONLY ELSE) = RETURNING EARLIER: If inside a function, no code below keyword "return" will run. Remember, the return will only happen if this condition is being met.
+    if (!letters.includes(event.key)) return; 
+    userGuess = event.key;
     // console.log(userGuess);
     // NOTE: docucment.querySelector = document.getElementById (new version with more options)
-    document.querySelector("#yourGuesses").textContent = userGuess;
+    document.querySelector('#yourGuesses').textContent += userGuess;
     guessesLeft--;
     
     document.querySelector('#guessesLeft').textContent = guessesLeft;
+    
+    console.log('userGuess is ' + userGuess);
+    console.log('compGuess is ' + compGuess);
 
 // Win Function
     if (userGuess === compGuess) {
         wins++;
         document.querySelector('#wins').textContent = wins;
-        // Reset Guesses Left to 7
-        guessesLeft = 7;
-        
+       
         // New Computer Choice (fresh batch of)
-        setComGuess(); 
+        resetGame(); 
     }
 // Loss Function
     else {
-        losses++;
-        document.querySelector('losses').textContent = losses;
-        guessesLeft = 7;
+        if (guessesLeft === 0) {
+            losses++;
+            document.querySelector('#losses').textContent = losses;
+            // User has lost and you....
+            resetGame();
+        }        
+
     }
 }
